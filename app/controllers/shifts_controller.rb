1adba35d9f09290fd05f9bd965cf69577e539a33
Yunus Sasmaz <yunus@squareup.com>
2014-04-15 10:41:42 -0700

    a minor change to variable names to make them more self explanatory

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,7 +1,7 @@
 class ShiftsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @shifts = Shift.all
+    @all_shifts = Shift.all
     @current_user_shifts = current_user.user_shifts
   end
 end
