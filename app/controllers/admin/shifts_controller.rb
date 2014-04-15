65b88d142794a503f05347c07322bcad94e5415f
Yunus Sasmaz <yunus@squareup.com>
2014-04-15 12:07:14 -0700

    created the method that updates assignments in the admin view

--- app/controllers/admin/shifts_controller.rb
+++ app/controllers/admin/shifts_controller.rb
@@ -5,11 +5,20 @@ class Admin::ShiftsController < ApplicationController
   def index
   end
 
-  # expected params: hash of shift ids to assigned user ids
-  # saves the new assignments for the employee
+  # expected params: array of assigned user_shift_ids
+  # saves the new assignments
   def update_assignments
-    assignments = {1=>1, 2=>1, 4=>2} # TODO: parse this info from the params (possibly from params[:assignments])
+    new_assignments = [1,3,4,6] # TODO: parse this info from the params (possibly from params[:assignments])
 
+    # clear the initial 'scheduled' flags
+    UserShift.where(scheduled: true).update_all(scheduled: false)
+
+    # TODO: Validations.
+    # Details: we are skipping a bunch of validations here.
+    # For instance, this code can be fooled into setting the 'scheduled' flags for two user_shifts that correspond to the same shift.
+
+    # set the 'scheduled' flags of new assignments
+    UserShift.find(new_assignments).update_all(scheduled: true)
   end
 
   private
