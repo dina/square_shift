bbb039a8925ff5e02f4efcfa892d960be3e6f6b1
Dina Westland <dina@squareup.com>
2014-04-14 18:42:23 -0700

    Seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# rake db:seed
+
+today = DateTime.now.utc.beginning_of_day
+sunday = today
+while sunday.cwday != 7
+  sunday += 1.day
+end
+
+User.create(
+  phone_number: "5555555555",
+  admin: true,
+  email: "admin@adminsquare.com",
+  password: "adminsRus"
+)
+
+0.upto(6) do |d|
+  day = sunday + d.days
+  Shift.create(start_at: day + 8.hours, end_at: day + 12.hours)
+  Shift.create(start_at: day + 12.hours, end_at: day + 16.hours)
+  Shift.create(start_at: day + 16.hours, end_at: day + 20.hours)
+end
