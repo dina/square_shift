dc3da38b282d2f27ed0e8a442b30fea36de45ec5
Yunus Sasmaz <yunus@squareup.com>
2014-04-16 15:44:46 -0700

    shift change request progress

--- app/models/shift_change_request.rb
+++ app/models/shift_change_request.rb
@@ -6,15 +6,20 @@ class ShiftChangeRequest < ActiveRecord::Base
   end
 
   belongs_to :initiator, class_name:"User"
+  belongs_to :original_user, class_name:"User"
+  belongs_to :original_shift, class_name:"UserShift"
   belongs_to :original_user_shift, class_name:"UserShift"
-
   belongs_to :target_user, class_name:"User"
+  belongs_to :target_shift, class_name:"Shift"
   belongs_to :target_user_shift, class_name:"UserShift"
 
 
   # initiator will always exist
-  def self.build(initiator, original_user_shift, target_user, target_user_shift)
-    req = new(initiator: initiator, original_user_shift: original_user_shift, target_user: target_user, target_user_shift: target_user_shift)
-    
+  def self.initiate(initiator, target_user, original_user_shift=nil, original_shift=nil, target_shift=nil)
+    # only an admin or the user themselves can request a change with that user set as origin
+    raise "Not Authorized" if !initiator.admin? and original_user_shift.user != initiator
+
+
+    create(initiator: initiator, target_user: target_user, original_user_shift: original_user_shift, target_user_shift: target_user_shift, status: Status::INITIATED)
   end
 end
