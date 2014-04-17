589fb289e065b07f7fd72426486a4d32c2f4e19b
Dina Westland <dina@squareup.com>
2014-04-17 14:52:44 -0700

    Notifications styling.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 module ApplicationHelper
 
-  def display_shift_time(start_datetime, end_datetime)
-    "#{display_datetime(start_datetime)} - #{display_datetime(end_datetime)}"
+  def display_shift_time(shift)
+    shift.start_at.strftime('%A, %I:%M%p - ') + shift.end_at.strftime('%I:%M%p')
   end
 
   def display_datetime(datetime)
     datetime.strftime('%m/%d %I:%M %p')
+  end
+
+  def pretty_date(datetime)
+    datetime.strftime('%A %I:%M%p')
   end
 
 end
