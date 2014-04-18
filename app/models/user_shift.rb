f788ce085ab7fdd5e646db5f4cddcbc8f4370a45
Yunus Sasmaz <yunus@squareup.com>
2014-04-18 11:09:25 -0700

    assets precompiled

--- app/models/user_shift.rb
+++ app/models/user_shift.rb
@@ -20,7 +20,7 @@ class UserShift < ActiveRecord::Base
     # For instance, this code can be fooled into setting the 'scheduled' flags for two user_shifts that correspond to the same shift.
 
     # set the 'scheduled' flags of new assignments
-    UserShift.find(new_assignment_ids).each(&:schedule!)
+    UserShift.where(id: new_assignment_ids).each(&:schedule!)
   end
 
   def self.generate_schedule!
