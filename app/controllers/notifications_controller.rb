025ce4b22fe96e0b1eac22ddbed7737c81c0e821
Dina Westland <dina@squareup.com>
2014-04-18 01:32:54 -0700

    jk, now it works

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,7 +5,7 @@ class NotificationsController < ApplicationController
   before_action :load_notifications, except: [:index]
 
   def index
-    @notifications = Notification.where(user: current_user).reverse
+    @notifications = Notification.where(user: current_user)
   end
 
   # Adds the shift to your schedule.
